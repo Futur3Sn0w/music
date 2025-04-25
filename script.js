51e2ad7275052665f1b2fe08e5b952a419a218b2
Futur3Sn0w <futur3sn0w@gmail.com>
2025-04-25 13:07:53 -0400

    moar
    not a lot of updates rn as i'm just working on making the site lol

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
     const imageFormats = ['jpg', 'jpeg', 'png'];
-    const container = $('<div class="music-container"></div>').appendTo('.right.half');
+    const container = $('.right.half');
+    let currentPath = [];
+    let musicData = null;
 
-    $.getJSON('source.json', function (data) {
-        data.productions.forEach(project => {
+    function renderEntries(entries) {
+        container.addClass('tempHide')
+
+        container.empty();
+        $('<button class="back-btn">⬅ Back</button>')
+            .toggle(currentPath.length > 0)
+            .click(() => {
+                currentPath.pop();
+                renderTree();
+            })
+            .appendTo(container);
+
+        entries.forEach(entry => {
             let imageFound = false;
             let imageUrl = '';
 
             for (let ext of imageFormats) {
-                const testUrl = `imgs/${project.id}.${ext}`;
+                const testUrl = `imgs/${entry.id}.${ext}`;
                 const xhr = new XMLHttpRequest();
                 xhr.open('HEAD', testUrl, false);
                 xhr.send();
@@ -22,39 +35,88 @@ $(document).ready(function () {
 
             const div = $('<div></div>', {
                 class: 'music-entry',
-                'title': project.title,
-                'data-description': project.description
+                'data-description': entry.description
             });
 
             if (imageFound) {
                 $('<img>', {
                     class: "albArt",
                     src: imageUrl,
-                    alt: project.title
+                    alt: entry.title
                 }).appendTo(div);
                 $('<img>', {
                     class: "backImg",
                     src: imageUrl,
-                    alt: project.title
+                    alt: entry.title
                 }).appendTo(div);
             }
 
-            $('<span>', {
-                class: "prodTitle",
-                'data-date': project.lastUpdate,
-                text: project.title
+            const prodInfo = $('<div>', {
+                class: "prodInfo"
             }).appendTo(div);
 
-            if (project.dataOpen) {
+            $('<span>', {
+                class: "prodTitle",
+                text: entry.title
+            }).appendTo(prodInfo);
+
+            $('<span>', {
+                class: "prodDate",
+                text: entry.lastUpdate
+            }).appendTo(prodInfo);
+
+            $('<span>', {
+                class: "prodDesc",
+                text: entry.description
+            }).appendTo(prodInfo);
+
+            if (entry.dataOpen) {
                 $('<button></button>', {
                     text: 'Play',
                     title: 'Opens music in new tab',
                     class: 'entry-btn',
-                    click: () => window.open(project.dataOpen, '_blank')
+                    click: () => window.open(entry.dataOpen, '_blank')
                 }).appendTo(div);
             }
 
             container.append(div);
         });
+
+        setTimeout(() => {
+            container.removeClass('tempHide')
+        }, 200);
+    }
+
+    function renderTree() {
+        container.addClass('tempHide')
+
+        setTimeout(() => {
+            container.empty();
+
+            if (currentPath.length === 0) {
+                $('<div class="tree-btn">🎵 Productions</div>')
+                    .click(() => {
+                        currentPath.push('productions');
+                        renderTree();
+                    })
+                    .appendTo(container);
+                $('<div class="tree-btn">🎧 Sets</div>')
+                    .click(() => {
+                        currentPath.push('sets');
+                        renderTree();
+                    })
+                    .appendTo(container);
+            } else {
+                const entries = musicData[currentPath[0]];
+                renderEntries(entries);
+            }
+
+            container.removeClass('tempHide')
+        }, 200);
+    }
+
+    $.getJSON('source.json', function (data) {
+        musicData = data;
+        renderTree();
     });
 });
